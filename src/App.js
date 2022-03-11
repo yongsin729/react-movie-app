86eda58819aa6551d1a5631c6433c312cbc767d1
yunghsin Lee <lenalee7963@gmail.com>
2022-03-11 16:24:40 +0900

    first commit1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button ,Card} from 'react-bootstrap';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+          영화 추천 블로그 
+      </div>
+
+      <Modal></Modal>
     </div>
   );
 }
 
+function Modal(){
+  return(
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+    // <div>
+    //   <h2>제목</h2>
+    //   <p>내용</p>
+    // </div>
+  )
+}
+
 export default App;
+
